3e1d8d68662c0ba76c0774693c715b3f7a813a98
matthewharrilal <matthewharrilal@gmail.com>
2017-09-07 18:04:56 -0700

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Problem 8 Solution to this problem Functions
+original_number = 0
+number = number + number[:-1]
+
+def fibonacci_iterative(num):
+
+    if num == int:
+        for items in len(num):
+            print(original_number)
+            print(number)
+    elif num != int:
+        for items in len(10):
+            print(original_number)
+            print(number)
